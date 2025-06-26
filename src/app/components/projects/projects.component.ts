e7aa66b915ab949d8c7fd903c19357667c9761ad
danielcamisi <danielcamillo2010@hotmail.com>
2025-06-26 18:28:30 -0300

    Images, pdf wass added and the session PROJECTS was placed

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+    downloadFile(url: string, fileName: string) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName; 
+    document.body.appendChild(link); 
+    link.click(); 
+    document.body.removeChild(link); 
+  }
 
+  redTo(url: string) {
+    window.open(url, '_blank');
+  }
 }
+
